ebff477fa2690f7891ba8935667f716f3d7ae5b1
unknown <jcf.daw@gmail.com>
2023-08-04 21:22:27 +0200

    adding about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../styles/about.css'
 
 const About = () => {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    setArticles(document.querySelectorAll('.article'))
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+
+      articles.forEach(article => {
+        const position = article.getBoundingClientReact().top;
+
+        if(position < windowHeight / 1.5) {
+          article.classList.add('show')
+        } else {
+          article.classList.remove('show')
+        }
+      })
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [articles])
+
   return (
-    <div className='about'></div>
+    <section className='about'>
+      <article>
+        <h2>¿Cómo funciona?</h2>
+        <p>Primero tienes que registrarte en la web para poder participar en los sorteos. Hay sorteos semanales y mensuales. Todos los sorteos tienen un cupo maximo de personas que pueden participar. ¡Tienes que estar atento!</p>
+      </article>
+      <article>
+        <h2>¿Cómo funciona?</h2>
+        <p>Primero tienes que registrarte en la web para poder participar en los sorteos. Hay sorteos semanales y mensuales. Todos los sorteos tienen un cupo maximo de personas que pueden participar. ¡Tienes que estar atento!</p>
+      </article>
+      <article>
+        <h2>¿Cómo funciona?</h2>
+        <p>Primero tienes que registrarte en la web para poder participar en los sorteos. Hay sorteos semanales y mensuales. Todos los sorteos tienen un cupo maximo de personas que pueden participar. ¡Tienes que estar atento!</p>
+      </article>
+      <article>
+        <h2>¿Cómo funciona?</h2>
+        <p>Primero tienes que registrarte en la web para poder participar en los sorteos. Hay sorteos semanales y mensuales. Todos los sorteos tienen un cupo maximo de personas que pueden participar. ¡Tienes que estar atento!</p>
+      </article>
+      <article>
+        <h2>¿Cómo funciona?</h2>
+        <p>Primero tienes que registrarte en la web para poder participar en los sorteos. Hay sorteos semanales y mensuales. Todos los sorteos tienen un cupo maximo de personas que pueden participar. ¡Tienes que estar atento!</p>
+      </article>
+    </section>
   )
 }
 
